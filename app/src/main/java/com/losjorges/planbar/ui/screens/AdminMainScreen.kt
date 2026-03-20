6d1ea2676880804a7076bddaf9de746f452e6499
jorgeeelp <24.damt.jleileon@fundacionmontessori.com>
2026-03-20 11:57:18 +0100

    logo

--- app/src/main/java/com/losjorges/planbar/ui/screens/AdminMainScreen.kt
+++ app/src/main/java/com/losjorges/planbar/ui/screens/AdminMainScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccessTimeFilled
 import androidx.compose.material.icons.filled.Build
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Person
